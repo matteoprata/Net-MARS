1916d5befed5cb026f0a612189e73b5c99a7195a
matteoprata <matteoprata1@gmail.com>
2022-07-07 09:02:49 +0200

    Revert "- ISR MULTI is fixed and works"
    This reverts commit 50adce8d5f400a2ab8e5c39bc38c8fb169d9ed6a.

--- src/main.py
+++ src/main.py
@@ -33,7 +33,7 @@ parser.add_argument('-gn', '--graph_name', type=str, default=original_config.gra
 config = None
 
 
-def save_stats_monotonous(stats, fname):
+def save_stats_as_df_ph1(stats, fname):
     """ saving number of repairs and flow routed """
     for i in stats:
         print(i)
@@ -60,71 +60,6 @@ def save_stats_monotonous(stats, fname):
     df["repairs"] = repairs
     df["iter"] = iter
     df["flow_cum"] = flow_cum
-
-    # position 0 we set the number of repairs
-    none_vec = [None]*len(flow_cum)
-
-    n_repairs_vector = none_vec[:]
-    n_repairs_vector[0] = n_repairs
-    df["n_repairs"] = n_repairs_vector
-
-    n_monitors_vector = none_vec[:]
-    n_monitors_vector[0] = len(stats[-1]["monitors"])
-    df["n_monitors"] = n_monitors_vector
-
-    n_monitor_msg_messages = none_vec[:]
-    n_monitor_msg_messages[0] = stats[-1]["packet_monitoring"]  # packet_monitor
-    df["n_monitor_msg"] = n_monitor_msg_messages
-
-    for k in demand_pairs:
-        df["d-" + str(k)] = demand_pairs[k]
-
-    print("saving stats > {}".format(fname))
-    df.to_csv("data/experiments/{}".format(fname))
-    return df
-
-
-def save_stats_NON_monotonous(stats, fname):
-    """ saving number of repairs and flow routed """
-    for i in stats:
-        print(i)
-
-    i_demand_pairs = stats[-1]["demands_sat"] if "demands_sat" in stats[-1].keys() else []
-    stopz = dict()
-    for k in i_demand_pairs:  # iterates demand edges
-        stop = len(i_demand_pairs[k]) - 1  # iteration indices
-        for ite_flow in reversed(i_demand_pairs[k]):
-            if ite_flow != i_demand_pairs[k][-1]:  # different from max_flow
-                break
-            stop -= 1
-        stopz[k] = stop+1
-        # print("ECCO", k, i_demand_pairs[k], stop)
-
-
-    repairs, iter, flow_cum = [], [], []
-    n_repairs = 0
-    demand_pairs = {k: [] for k in stats[-1]["demands_sat"].keys()}
-    for i, dic in enumerate(stats):  # iteration index
-        vals = dic["node"] + dic["edge"]
-        # numbers in this iteration, to propagate values accordingly
-        n_vals = max(len(vals), 1)
-        repairs += vals if len(vals) > 0 else [None]
-        iter += [dic["iter"]] * n_vals
-        flow_cum += [stats[i]["flow"]] * n_vals  # IGNORED
-        n_repairs += len(vals)
-
-        i_demand_pairs = stats[i]["demands_sat"] if "demands_sat" in stats[i].keys() else []
-        for k in i_demand_pairs:  # iterates demand edges
-            d_flow = [0] * n_vals
-            d_flow[-1] = stats[i]["demands_sat"][k][i] if i == stopz[k] else 0
-            demand_pairs[k] = demand_pairs[k] + d_flow
-
-    df = pd.DataFrame()
-    df["repairs"] = repairs
-    df["iter"] = iter
-
-    flows = np.array([i for i in demand_pairs.values()]).T
-    df["flow_cum"] = np.sum(np.cumsum(flows, axis=0), axis=1)
 
     # position 0 we set the number of repairs
     none_vec = [None]*len(flow_cum)
@@ -261,10 +196,7 @@ def run_var_seed_dis(seed, dis, budget, nnodes, flowpp, rep_mode, pick_mode, mon
         enable_print()
 
         if stats is not None:
-            if config.algo_name in [co.AlgoName.SHP, co.AlgoName.ISR_MULTICOM]:
-                df = save_stats_NON_monotonous(stats, fname)
-            else:
-                df = save_stats_monotonous(stats, fname)
+            df = save_stats_as_df_ph1(stats, fname)
             print(df.to_string())
     else:
         print()
@@ -373,8 +305,8 @@ if __name__ == '__main__':
     # parallel_exec(seeds=seeds)
 
     # seed, dis, budget, nnodes, flowpp, rep_mode, pick_mode, monitor_placement, indvar, monitoring_type, algo_name, is_parallel=False
-    v = (99, .5, 16, 7, 11, co.ProtocolRepairingPath.MIN_COST_BOT_CAP,
+    v = (952, .5, 16, 7, 11, co.ProtocolRepairingPath.MIN_COST_BOT_CAP,
                              co.ProtocolPickingPath.MIN_COST_BOT_CAP, co.ProtocolMonitorPlacement.BUDGET, co.IndependentVariable.N_DEMAND_EDGES,
-                             co.PriorKnowledge.TOMOGRAPHY, co.AlgoName.CEDARNEW, False)
+                             co.PriorKnowledge.TOMOGRAPHY, co.AlgoName.ISR, False)
     run_var_seed_dis(*v)
 
